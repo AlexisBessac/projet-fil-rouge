885141366a71777be1ef69b9b4f0df12cebc0b40
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-02-28 15:40:56 +0100

    nouveau commit

---
+++ 404.php
@@ -0,0 +1,5 @@
+<?php require 'templates/header.html.php';
+
+require 'templates/404.html.php';
+
+require 'templates/footer.html.php' ?>
---
+++ templates/404.html.php
@@ -0,0 +1,8 @@
+<?php require 'templates/header.html.php' ?>
+
+<div class="container">
+    <h1>Erreur 404</h1>
+    <p>Votre page semble être perdue.</p>
+</div>
+
+<?php require 'templates/footer.html.php' ?>
