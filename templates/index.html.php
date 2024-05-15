03258a3ad823a8558352d58f2a728c86a2d4e4ec
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-05-15 18:52:22 +0200

    HTML/CSS de la page d'accueil

--- templates/index.html.php
+++ templates/index.html.php
@@ -1,1 +1,6 @@
-<h1>Hello World</h1>
+<div class="container">
+    <h1>Formul'Air</h1>
+    <div class="punchline">
+        <p>À tous les coups, Formul'Air : votre souffle de facilité pour suivre vos dossiers d'inscription avec légèreté et rapidité</p>
+    </div>
+</div>
