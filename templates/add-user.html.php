eb7162ae1f60e74841f3cc9741c9f87955de8da3
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-05-15 11:20:55 +0200

    Affichage des mesages d'erreurs dans le template  HTML

--- templates/add-user.html.php
+++ templates/add-user.html.php
@@ -5,34 +5,58 @@
         <div>
             <label for="firstname">Prénom</label>
             <input type="text" name="firstname" id="firstname" placeholder="Votre Prénom">
+            <?php if(isset($errors) && !empty($errors['firstname'])): ?>
+                <div><?= $errors['firstname'] ?></div>
+            <?php endif; ?>
         </div>
         <div>
             <label for="lastname">Nom</label>
             <input type="text" name="lastname" id="lastname" placeholder="Votre nom">
+            <?php if(isset($errors) && !empty($errors['lastname'])): ?>
+                <div><?= $errors['lastname'] ?></div>
+            <?php endif; ?>
         </div>
         <div>
             <label for="email">Email</label>
             <input type="email" name="email" id="email" placeholder="">
+            <?php if(isset($errors) && !empty($errors['email'])): ?>
+                <div><?= $errors['email'] ?></div>
+            <?php endif; ?>
         </div>
         <div>
             <label for="password">Mot de passe</label>
             <input type="password" name="password" id="password">
+            <?php if(isset($errors) && !empty($errors['password'])): ?>
+                <div><?= $errors['password'] ?></div>
+            <?php endif; ?>
         </div>
         <div>
             <label for="phone_number">Téléphone</label>
             <input type="tel" name="phone_number" id="phone_number">
+            <?php if(isset($errors) && !empty($errors['phone_number'])): ?>
+                <div><?= $errors['phone_number'] ?></div>
+            <?php endif; ?>
         </div>
         <div>
             <label for="adress">Adresse</label>
-            <input type="text" name="adress" id="adress" placeholder="Entrer une adresse">
+            <input type="text" name="address" id="address" placeholder="Entrer une adresse">
+            <?php if(isset($errors) && !empty($errors['address'])): ?>
+                <div><?= $errors['adress'] ?></div>
+            <?php endif; ?>
         </div>
         <div>
             <label for="zip_code">Code Postal</label>
             <input type="number" name="zip_code" id="zip_code">
+            <?php if(isset($errors) && !empty($errors['zip_code'])): ?>
+                <div><?= $errors['zip_code'] ?></div>
+            <?php endif; ?>
         </div>
         <div>
             <label for="city">Ville</label>
             <input type="text" name="city" id="city" placeholder="Entrer une ville">
+            <?php if(isset($errors) && !empty($errors['city'])): ?>
+                <div><?= $errors['city'] ?></div>
+            <?php endif; ?>
         </div>
     </form>
     <div>
