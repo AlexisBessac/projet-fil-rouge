670ea0e848718956bcd6b7014bbc7c3f4d2637b5
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-05-16 16:32:21 +0200

    fin des modifications pour la page des formations

--- src/pages/lessons.php
+++ src/pages/lessons.php
@@ -3,6 +3,9 @@
 # Connexion à la base de données
 require '../src/data/db-connect.php';
 
-# Récupération de tous les utilisateur
+# Récupération de toutes les formations
 $query = $dbh->query("SELECT * FROM formation");
 $lesson = $query->fetchAll();
+
+$title = "Liste des formations";
+$description = "La page liste les formations disponibles dans l'application ";
