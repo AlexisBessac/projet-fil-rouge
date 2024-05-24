9ba530d5ddee5630ca6322354ee1b1118ab141da
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-05-24 16:50:24 +0200

    Tentative de connexion

--- src/pages/connexion.php
+++ src/pages/connexion.php
@@ -1,25 +1,24 @@
 <?php
 
-
 $title = "Se Connecter";
 $description = "Page de connexion à Formul'Air";
 
 // Vérifie que le formulaire a bien été envoyé
-if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form_login_submit']))
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form_login_submit']))
 {
     $errors = [];
 
-    if(empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
+    if (empty($_POST['email']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
     {
         $errors['email'] = 'Le champ Email est obligatoire et doit être une adresse email valide';
     }
 
-    if(empty($_POST['password']))
+    if (empty($_POST['password']))
     {
         $errors['password'] = 'Le Mot de passe est obligatoire.';
     }
 
-    if(empty($errors))
+    if (empty($errors)) 
     {
         require '../src/data/db-connect.php';
 
@@ -29,34 +28,30 @@ if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['form_login_submit']))
         $query->execute(['email' => $email]);
         $user = $query->fetch();
 
-        if($user)
+        if ($user) 
         {
-            // Assurez-vous d'utiliser le même sel que celui utilisé lors de l'enregistrement du mot de passe
-            $salt = "fil-rouge";
-            $password = $_POST['password'] . $salt;
+            $salt = "fil-rouge"; // Sel utilisé lors de la création du hash de mot de passe
+            $password = $_POST['password'] . $salt; // Concaténation du sel avec le mot de passe saisi
+            $hashed_password = $user['password'];
 
-            if(password_verify($password, $user['password']))
+            if (password_verify($password, $hashed_password)) 
             {
                 // Authentification réussie
-                // Ouverture de la session
                 session_start();
-                $_SESSION['id_utilisateur'] = $user['id_utilisateur']; // Utilisez $user['id'] au lieu de $customer['id_customer']
-
+                $_SESSION['id_utilisateur'] = $user['id_utilisateur'];
                 header('Location: /?page=dashboard');
                 exit;
             }
-            else
+            else 
             {
                 $errors['email'] = 'Email ou mot de passe incorrect';
                 $errors['password'] = 'Email ou mot de passe incorrect';
-            }   
-
-        }
-        else
+            }
+        } 
+        else 
         {
             $errors['email'] = 'Email ou mot de passe incorrect';
             $errors['password'] = 'Email ou mot de passe incorrect';
         }
-
-    }   
+    }
 }
