4dca920de7f331618abe6520d49c16f2e28a528d
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-05-24 15:36:31 +0200

    Refonte des formulaires du CRUD utilisateur

--- templates/add-user.html.php
+++ templates/add-user.html.php
@@ -1,45 +1,44 @@
 <div class="container">
-    <h1>S'inscrire</h1>
+    <h1>Ajouter un utilisateur</h1>
+    <a href="/?page=users"><button class="button cancel-button">Annuler</button></a>
     <form action="" method="POST">
-        <div class="form-row">
-            <div class="form-group">
-                <label for="firstname">Prénom</label>
-                <input type="text" name="firstname" id="firstname" placeholder="Votre Prénom">
-                <?php if (isset($errors) && !empty($errors['firstname'])) : ?>
-                    <div class="ajout-error"><?= htmlspecialchars($errors['firstname']) ?></div>
-                <?php endif; ?>
-            </div>
-            <div class="form-group">
-                <label for="lastname">Nom</label>
-                <input type="text" name="lastname" id="lastname" placeholder="Votre nom">
-                <?php if (isset($errors) && !empty($errors['lastname'])) : ?>
-                    <div class="ajout-error"><?= htmlspecialchars($errors['lastname']) ?></div>
-                <?php endif; ?>
-            </div>
-            <div class="form-group">
-                <label for="phone_number">Téléphone</label>
-                <input type="tel" name="phone_number" id="phone_number">
-                <?php if (isset($errors) && !empty($errors['phone_number'])) : ?>
-                    <div class="ajout-error"><?= htmlspecialchars($errors['phone_number']) ?></div>
-                <?php endif; ?>
-            </div>
+        <div>
+            <label for="firstname">Prénom</label>
+            <input type="text" name="firstname" id="firstname" placeholder="Votre Prénom">
+            <?php if (isset($errors) && !empty($errors['firstname'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['firstname']) ?></div>
+            <?php endif; ?>
         </div>
-        <div class="form-row">
-            <div class="form-group">
+        <div>
+            <label for="lastname">Nom</label>
+            <input type="text" name="lastname" id="lastname" placeholder="Votre nom">
+            <?php if (isset($errors) && !empty($errors['lastname'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['lastname']) ?></div>
+            <?php endif; ?>
+        </div>
+        <div class="coordonne">
+            <label for="phone_number">Téléphone</label>
+            <input type="tel" name="phone_number" id="phone_number">
+            <?php if (isset($errors) && !empty($errors['phone_number'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['phone_number']) ?></div>
+            <?php endif; ?>
+        </div>
+        <div class="adresse-form">
+            <div>
                 <label for="street_number">Numéro de la rue</label>
                 <input type="text" name="street_number" id="street_number">
                 <?php if (isset($errors) && !empty($errors['street_number'])) : ?>
                     <div class="ajout-error"><?= htmlspecialchars($errors['street_number']) ?></div>
                 <?php endif; ?>
             </div>
-            <div class="form-group">
+            <div>
                 <label for="street">Voie</label>
                 <input type="text" name="street" id="street" placeholder="Entrer le nom d'une rue">
                 <?php if (isset($errors) && !empty($errors['street'])) : ?>
                     <div class="ajout-error"><?= htmlspecialchars($errors['street']) ?></div>
                 <?php endif; ?>
             </div>
-            <div class="form-group">
+            <div>
                 <label for="zip_code">Code Postal</label>
                 <input type="number" name="zip_code" id="zip_code">
                 <?php if (isset($errors) && !empty($errors['zip_code'])) : ?>
@@ -47,29 +46,40 @@
                 <?php endif; ?>
             </div>
         </div>
-        <div class="form-row">
-            <div class="form-group">
-                <label for="city">Ville</label>
-                <input type="text" name="city" id="city" placeholder="Entrer une ville">
-                <?php if (isset($errors) && !empty($errors['city'])) : ?>
-                    <div class="ajout-error"><?= htmlspecialchars($errors['city']) ?></div>
-                <?php endif; ?>
-            </div>
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" name="email_register" id="email_register" placeholder="">
-                <?php if (isset($errors) && !empty($errors['email_register'])) : ?>
-                    <div class="ajout-error"><?= htmlspecialchars($errors['email_register']) ?></div>
-                <?php endif; ?>
-            </div>
-            <div class="form-group">
-                <label for="password">Mot de passe</label>
-                <input type="password" name="password" id="password">
-                <?php if (isset($errors) && !empty($errors['password'])) : ?>
-                    <div class="ajout-error"><?= htmlspecialchars($errors['password']) ?></div>
-                <?php endif; ?>
-            </div>
+        <div class="coordonne">
+            <label for="city">Ville</label>
+            <input type="text" name="city" id="city" placeholder="Entrer une ville">
+            <?php if (isset($errors) && !empty($errors['city'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['city']) ?></div>
+            <?php endif; ?>
         </div>
-        <input type="submit" class="button ajout-button" name="form_inscribe_submit" value="S'inscrire">
+        <div class="coordonne">
+            <label for="email">Email</label>
+            <input type="email" name="email_register" id="email_register" placeholder="">
+            <?php if (isset($errors) && !empty($errors['email_register'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['email_register']) ?></div>
+            <?php endif; ?>
+        </div>
+        <div>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password">
+            <?php if (isset($errors) && !empty($errors['password'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['password']) ?></div>
+            <?php endif; ?>
+        </div>
+        <div class="role-utilisateur">
+            <label for="role_id">Role de l'utilisateur</label>
+            <?php foreach ($roles as $role) : ?>
+                <?php $is_admin = trim(strtolower($role['nom_role'])) === 'administrateur'; ?>
+                <div class="<?= $is_admin ? 'hidden' : '' ?>">
+                    <label for="role_id_<?= htmlspecialchars($role['Id_role']) ?>"><?= htmlspecialchars($role['nom_role']) ?></label>
+                    <input type="radio" name="role_id" id="role_id_<?= htmlspecialchars($role['Id_role']) ?>" value="<?= htmlspecialchars($role['Id_role']) ?>">
+                </div>
+            <?php endforeach; ?>
+            <?php if (isset($errors) && !empty($errors['role_id'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['role_id']) ?></div>
+            <?php endif; ?>
+        </div>
+        <input type="submit" class="button register-button" title="Ajouter un utilisateur" name="add_user_submit" value="Ajouter un utilisateur"></input>
     </form>
 </div>
--- templates/edit-user.html.php
+++ templates/edit-user.html.php
@@ -1,7 +1,7 @@
 <div class="container">
-<h1>Modification d'un utilisateur</h1>
+    <h1>Modifier un utilisateur</h1>
     <a href="/?page=users"><button class="button cancel-button">Annuler</button></a>
-    <form method="POST">
+    <form action="" method="POST">
         <div>
             <label for="firstname">Prénom</label>
             <input type="text" name="firstname" id="firstname" placeholder="Votre Prénom">
@@ -16,58 +16,70 @@
                 <div class="ajout-error"><?= htmlspecialchars($errors['lastname']) ?></div>
             <?php endif; ?>
         </div>
-        <div>
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="">
-            <?php if (isset($errors) && !empty($errors['email'])) : ?>
-                <div class="ajout-error"><?= htmlspecialchars($errors['email']) ?></div>
-            <?php endif; ?>
-        </div>
-        <div>
+        <div class="coordonne">
             <label for="phone_number">Téléphone</label>
             <input type="tel" name="phone_number" id="phone_number">
             <?php if (isset($errors) && !empty($errors['phone_number'])) : ?>
                 <div class="ajout-error"><?= htmlspecialchars($errors['phone_number']) ?></div>
             <?php endif; ?>
         </div>
-        <div>
-            <label for="street_number">Numéro de la rue</label>
-            <input type="text" name="street_number" id="street_number">
-            <?php if (isset($errors) && !empty($errors['street_number'])) : ?>
-                <div class="ajout-error"><?= htmlspecialchars($errors['street_number']) ?></div>
-            <?php endif; ?>
+        <div class="adresse-form">
+            <div>
+                <label for="street_number">Numéro de la rue</label>
+                <input type="text" name="street_number" id="street_number">
+                <?php if (isset($errors) && !empty($errors['street_number'])) : ?>
+                    <div class="ajout-error"><?= htmlspecialchars($errors['street_number']) ?></div>
+                <?php endif; ?>
+            </div>
+            <div>
+                <label for="street">Voie</label>
+                <input type="text" name="street" id="street" placeholder="Entrer le nom d'une rue">
+                <?php if (isset($errors) && !empty($errors['street'])) : ?>
+                    <div class="ajout-error"><?= htmlspecialchars($errors['street']) ?></div>
+                <?php endif; ?>
+            </div>
+            <div>
+                <label for="zip_code">Code Postal</label>
+                <input type="number" name="zip_code" id="zip_code">
+                <?php if (isset($errors) && !empty($errors['zip_code'])) : ?>
+                    <div class="ajout-error"><?= htmlspecialchars($errors['zip_code']) ?></div>
+                <?php endif; ?>
+            </div>
         </div>
-        <div>
-            <label for="street">Voie</label>
-            <input type="text" name="street" id="street" placeholder="Entrer le nom d'une rue">
-            <?php if (isset($errors) && !empty($errors['street'])) : ?>
-                <div class="ajout-error"><?= htmlspecialchars($errors['street']) ?></div>
-            <?php endif; ?>
-        </div>
-        <div>
-            <label for="zip_code">Code Postal</label>
-            <input type="number" name="zip_code" id="zip_code">
-            <?php if (isset($errors) && !empty($errors['zip_code'])) : ?>
-                <div class="ajout-error"><?= htmlspecialchars($errors['zip_code']) ?></div>
-            <?php endif; ?>
-        </div>
-        <div>
+        <div class="coordonne">
             <label for="city">Ville</label>
             <input type="text" name="city" id="city" placeholder="Entrer une ville">
             <?php if (isset($errors) && !empty($errors['city'])) : ?>
                 <div class="ajout-error"><?= htmlspecialchars($errors['city']) ?></div>
             <?php endif; ?>
         </div>
+        <div class="coordonne">
+            <label for="email">Email</label>
+            <input type="email" name="email_register" id="email_register" placeholder="">
+            <?php if (isset($errors) && !empty($errors['email_register'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['email_register']) ?></div>
+            <?php endif; ?>
+        </div>
         <div>
+            <label for="password">Mot de passe</label>
+            <input type="password" name="password" id="password">
+            <?php if (isset($errors) && !empty($errors['password'])) : ?>
+                <div class="ajout-error"><?= htmlspecialchars($errors['password']) ?></div>
+            <?php endif; ?>
+        </div>
+        <div class="role-utilisateur">
             <label for="role_id">Role de l'utilisateur</label>
-            <?php foreach($roles as $role) : ?>
-                <label for="role_id <?= $role['Id_role'] ?>"><?= $role['nom_role'] ?></label>
-                <input type="radio" name="role_id" id="role_id <?= $role['Id_role'] ?>" value="<?= $role['Id_role'] ?>">
+            <?php foreach ($roles as $role) : ?>
+                <?php $is_admin = trim(strtolower($role['nom_role'])) === 'administrateur'; ?>
+                <div class="<?= $is_admin ? 'hidden' : '' ?>">
+                    <label for="role_id_<?= htmlspecialchars($role['Id_role']) ?>"><?= htmlspecialchars($role['nom_role']) ?></label>
+                    <input type="radio" name="role_id" id="role_id_<?= htmlspecialchars($role['Id_role']) ?>" value="<?= htmlspecialchars($role['Id_role']) ?>">
+                </div>
             <?php endforeach; ?>
             <?php if (isset($errors) && !empty($errors['role_id'])) : ?>
                 <div class="ajout-error"><?= htmlspecialchars($errors['role_id']) ?></div>
             <?php endif; ?>
         </div>
-        <input type="submit" class="button edit_user_button" name="edit_user_submit" value="Modifier"></input>
+        <input type="submit" class="button edit_user_button" title="Modifier un utilisateur" name="edit_user_submit" value="Modifier un utilisateur"></input>
     </form>
 </div>
