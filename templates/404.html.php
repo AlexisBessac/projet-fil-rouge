d8c18eea40a6bc22e00f8c47db26d9e4d74425c6
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-05-27 16:13:17 +0200

    Refonte de la page 404

--- templates/404.html.php
+++ templates/404.html.php
@@ -1,5 +1,7 @@
 <div class="container">
     <h1>Erreur 404</h1>
-    <p class="error">La page est en attente de traitement</p>
-    <a href="/" class="error">Retour à la page d'accueil</a>
+    <div class="error">
+        <p>La page est en attente de traitement</p>
+        <a href="/">Retour à la page d'accueil</a>
+    </div>
 </div>
