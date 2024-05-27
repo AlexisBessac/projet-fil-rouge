c92a0a33592392e58f3c3b29b0a86f3c48e83920
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-05-27 16:26:58 +0200

    Dernière refonte de la page d'Erreur 404

--- templates/404.html.php
+++ templates/404.html.php
@@ -1,7 +1,8 @@
 <div class="container">
-    <h1>Erreur 404</h1>
     <div class="error">
+        <h1>Erreur 404</h1>
         <p>La page est en attente de traitement</p>
         <a href="/">Retour à la page d'accueil</a>
     </div>
+    <img src="../assets/image/pagenontrouvee.jpg" alt="Empilement de dossier">
 </div>
