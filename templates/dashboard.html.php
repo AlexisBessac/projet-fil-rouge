483d99ea0e8f6b461c9b4b3bd6b2d4ed7ed8325c
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-05-31 11:48:24 +0200

    La déconnexion de l'utilisateur fonctionne

--- templates/dashboard.html.php
+++ templates/dashboard.html.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h1>Mon  tableau de bord</h1>
     <div class="button-container">
-        <a href="/?page=connexion" class="button">se Déconnecter</a>
+        <a href="/?page=connexion"><button class="button">Se Déconnecter</button></a>
     </div>
 </div>
