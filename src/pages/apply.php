f88ac4114c121d0085ca4d75c0580d019ade9266
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-06-06 11:49:21 +0200

    Première tentative pour faire apparaître les documents à fournir pour une formation

--- src/pages/apply.php
+++ src/pages/apply.php
@@ -2,3 +2,14 @@
 
 $title = "Document à fournir pour une formation";
 $description = "Sur cette page on trouve la liste des documents à fournir pour chaque formation";
+
+require '../src/data/db-connect.php';
+
+$query = $dbh->prepare("SELECT t.nom_type_document 
+                        FROM type_de_document t 
+                        INNER JOIN formation_type_document ftd ON t.id_type = ftd.id_type 
+                        INNER JOIN formation f ON ftd.id_formation = f.id_formation 
+                        WHERE f.id_formation = ?");
+
+    $query->execute(['id_formation']);
+    $doc = $query->FETCHALL();
--- templates/apply.html.php
+++ templates/apply.html.php
@@ -1,4 +1,18 @@
 <div class="container">
     <h1>Liste des documents à fournir pour cette formation</h1>
     <a href="/?page=dashboard"><button class="button">Revenir sur mon tableau de bord</button></a>
+    <table class="table">
+    <thead>
+        <tr>
+            <th>Nom du document</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($doc as $docs) : ?>
+        <tr>
+            <td><?=$docs['nom_type_document'] ?></td>
+        </tr>
+        <?php endforeach; ?>
+    </tbody>
+</table>
 </div>
