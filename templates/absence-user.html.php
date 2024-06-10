23cb7fb9ec4b552e16ad879f2efe401a2cf971eb
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-06-10 10:32:03 +0200

    Suite de la page asbence

--- templates/absence-user.html.php
+++ templates/absence-user.html.php
@@ -4,8 +4,14 @@
     <section>
         <div>
             <form action="" method="POST">
-                <label for="user_file">Parcourir</label>
-                <input type="file" name="user_file" id="user_File">
+                <div>
+                    <label for="user_file">Parcourir</label>
+                    <input type="file" name="user_file" id="user_File">
+                </div>
+                <div>
+                    <label for="absence-text">Motif de l'absence</label>
+                    <input type="text" name="absence-text" id="absence-text" placeholder="Maladie">
+                </div>
             </form>
         </div>
     </section>
@@ -16,7 +22,7 @@
     </section>
     <section>
         <div>
-            <button class="button" name="user_file" type="submit" title="Trasmettre mon absnce">Transmettre mon absence</button>
+            <button class="button reguster-buttonr" name="user_file" type="submit" title="Trasmettre mon absnce">Transmettre mon absence</button>
         </div>
     </section>
 </div>
