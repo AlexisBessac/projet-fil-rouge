f4c595601f558f9a5b5f51d37042b0bef8521cdb
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-06-27 15:14:21 +0200

    Refinte du retard utilisateur

--- templates/late-user.html.php
+++ templates/late-user.html.php
@@ -1,6 +1,6 @@
 <div class="container">
     <h1>Déclarer un retard</h1>
-    <a href="/?page=dashboard"><button class="button" title="Revenir sur mon tableau de bord">Revenir sur mon tableau de bord</button></a> 
+    <a href="/?page=dashboard"><button class="button" title="Revenir sur mon tableau de bord">Revenir sur mon tableau de bord</button></a>
     <section>
         <div>
             <form action="" method="POST" enctype="multipart/form-data" class="form-absence">
@@ -9,16 +9,16 @@
                     <input type="file" name="user_file" id="user_file">
                 </div>
                 <div>
-                    <label for="absence-text">Motif du retard</label>
-                    <input type="text" name="absence-text" id="absence-text" placeholder="Maladie">
+                    <label for="late-text">Motif du retard</label>
+                    <input type="text" name="late-text" id="late-text" placeholder="Ex. Maladie">
                 </div>
                 <div>
-                    <label for="date_debut_absence">Date du retard</label>
-                    <input type="date" name="date_debut_absence" id="date_debut_absence">
+                    <label for="date_retard">Date du retard</label>
+                    <input type="date" name="date_retard" id="date_retard">
                 </div>
                 <div class="dropzone">Où faites glisser vos documents ici</div>
                 <div>
-                    <button class="button reguster-buttonr" name="submit_form_upload" type="submit" title="Trasmettre mon retard">Transmettre mon retard</button>
+                    <button class="button reguster-buttonr" name="submit_form_late" type="submit" title="Trasmettre mon retard">Transmettre mon retard</button>
                 </div>
             </form>
         </div>
