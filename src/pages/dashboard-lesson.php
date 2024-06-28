132e751567b1816f79712a4fe766b1f7ec4012f2
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-06-28 15:44:09 +0200

    Fin du dashboard du gestionnaire administratif

--- src/pages/dashboard-lesson.php
+++ src/pages/dashboard-lesson.php
@@ -3,3 +3,10 @@
 $title = "Tableau de bord du responsable administratif";
 $description = "Sur cette page, le responsable administratif peut voir les documents envoyés par un candidat dans le cadre d'une inscription à une formation, d'une absence ou d'un retard et les valider ou non";
 
+require '../src/data/db-connect.php';
+
+$query= $dbh->prepare(" SELECT prenom, nom, date_retard, motif_retard, justificatif_retard, duree_prevue 
+                        FROM retard 
+                        JOIN utilisateur ON retard.id_utilisateur = utilisateur.id_utilisateur;");
+$query->execute();
+$late = $query->fetchAll();
--- templates/dashboard-lesson.html.php
+++ templates/dashboard-lesson.html.php
@@ -3,4 +3,42 @@
     <div class="button-container">
         <a href="/?page=connexion"><button class="button" title="Se déconnecter">Se Déconnecter</button></a>
     </div>
+    <section>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Prénom</th>
+                    <th>Nom</th>
+                    <th>Raison du retard</th>
+                    <th>Date du retard</th>
+                    <th>Durée du retard</th>
+                    <th>Justificatif</th>
+                    <th colspan="2">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($late as $lates) : ?>
+                    <tr>
+                        <td><?= $lates['prenom'] ?></td>
+                        <td><?= $lates['nom'] ?></td>
+                        <td><?= $lates['motif_retard'] ?></td>
+                        <td><?= date('d-m-Y', strtotime($lates['date_retard'])) ?></td>
+                        <td><?= date('H:i', strtotime($lates['duree_prevue'])) ?></td>
+                        <td><?= $lates['justificatif_retard'] ?></td>
+                        <td>
+                            <button class="button" title="Justifié">Justifié</button>
+                        </td>
+                        <td>
+                            <button class="button" title="Injustifié">Injustifié</button>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                <?php if (empty($late)) : ?>
+                    <tr>
+                        <td colspan="8" class="text-user">Aucun retard présent dans la base de données</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </section>
 </div>
