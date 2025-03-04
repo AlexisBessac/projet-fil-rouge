2b8840123840791e4e7557339aa66e422cedbb74
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2025-03-04 18:05:53 +0100

    Début de la refonte du CSS des CRUD

--- templates/lessons.html.php
+++ templates/lessons.html.php
@@ -1,49 +1,51 @@
-<section class="container">
-    <h1>Liste des formations</h1>
-    <div>
+<section class="container py-5">
+    <h1 class="text-center my-4">Liste des formations</h1>
+    <div class="my-4">
         <a href="/?page=add-lesson"><button class="button" title="Créer une formation ">Créer une formation</button></a>
     </div>
-    <div>
+    <div class="my-4">
         <a href="/?page=users"><button class="lesson" title="Liste des utilisateurs">Liste des utilisateurs</button></a>
     </div>
     <div class="button-container">
         <a href="/?page=connexion"><button class="button" title="Se Déconnecter">Se Déconnecter</button></a>
     </div>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nom de la formation</th>
-                <th>Date de début</th>
-                <th>Date de fin</th>
-                <th>Lieu</th>
-                <th>Ville</th>
-                <th colspan="2">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($lesson as $lessons) : ?>
+    <div class="my-4">
+        <table class="table table-bordered table-striped">
+            <thead>
                 <tr>
-                    <td><?= $lessons['nom_formation'] ?></td>
-                    <td><?= $lessons['date_debut'] ?></td>
-                    <td><?= $lessons['date_fin'] ?></td>
-                    <td><?= $lessons['lieu'] ?></td>
-                    <td><?= $lessons['ville'] ?></td>
-                    <td>
-                        <a href="/?page=edit-lesson&id=<?= $lessons['id_formation'] ?>" class="button" title="Modifier">Modifier</a>
-                    </td>
-                    <td>
-                        <form action="/?page=delete-lesson" method="POST" onsubmit="return confirm('Are you sure ?')">
-                            <input type="hidden" name="lesson_id" value="<?= $lessons['id_formation'] ?>" />
-                            <button class="delete__button" type="submit" title="Supprimer">Supprimer</button>
-                        </form>
-                    </td>
+                    <th>Nom de la formation</th>
+                    <th>Date de début</th>
+                    <th>Date de fin</th>
+                    <th>Lieu</th>
+                    <th>Ville</th>
+                    <th colspan="2">Actions</th>
                 </tr>
-            <?php endforeach; ?>
-            <?php if (empty($lessons)) : ?>
-                <tr>
-                    <td colspan="7" class="text-user">Aucune formation présente dans la base de donnée</td>
-                </tr>
-            <?php endif; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($lesson as $lessons) : ?>
+                    <tr>
+                        <td><?= $lessons['nom_formation'] ?></td>
+                        <td><?= $lessons['date_debut'] ?></td>
+                        <td><?= $lessons['date_fin'] ?></td>
+                        <td><?= $lessons['lieu'] ?></td>
+                        <td><?= $lessons['ville'] ?></td>
+                        <td>
+                            <a href="/?page=edit-lesson&id=<?= $lessons['id_formation'] ?>" class="btn button" title="Modifier">Modifier</a>
+                        </td>
+                        <td>
+                            <form action="/?page=delete-lesson" method="POST" onsubmit="return confirm('Are you sure ?')">
+                                <input type="hidden" name="lesson_id" value="<?= $lessons['id_formation'] ?>" />
+                                <button class="btn btn-danger" type="submit" title="Supprimer">Supprimer</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                <?php if (empty($lessons)) : ?>
+                    <tr>
+                        <td colspan="7" class="text-center">Aucune formation présente dans la base de donnée</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
 </section>
--- templates/users.html.php
+++ templates/users.html.php
@@ -1,56 +1,62 @@
-<section class="container">
-    <h1>Liste des utilisateurs</h1>
-    <div>
+<section class="container py-5">
+    <h1 class="text-center my-4">Liste des utilisateurs</h1>
+    <div class="my-4">
         <a href="/?page=add-user"><button class="button" title="Créer un utilisateur">Créer un utilisateur</button></a>
     </div>
-    <nav class="navbar">
-        <ul>
-            <li><a href="/?page=lessons"><button class="lesson" title="Liste des Formations">Liste des Formations</button></a></li>
-            <li><a href="/?page=docs"><button class="lesson" title="Documents à fournir dans le cadre d'une formation">Document à fournir dans le cadre d'une formation</button></a></li>
+    <nav class="navbar navbar-expand-lg navbar-light my-4 justify-content-center">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="/?page=lessons"><button class="button" title="Liste des formations">Liste des formations</button></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/?page=docs"><button class="button" title="Liste des documents à fournir pour une formation">Liste des documents à fournir pour une formation</button></a>
+            </li>
         </ul>
     </nav>
     <div class="button-container">
         <a href="/?page=connexion"><button class="button" title="Se Déconnecter">Se Déconnecter</button></a>
     </div>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Prénom</th>
-                <th>Nom</th>
-                <th>Email</th>
-                <th>Numéro</th>
-                <th>Rue</th>
-                <th>Code Postal</th>
-                <th>Ville</th>
-                <th colspan="2">Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($user as $users) : ?>
+    <div class="my-4">
+        <table class="table table-striped table-bordered">
+            <thead>
                 <tr>
-                    <td><?= $users['prenom'] ?></td>
-                    <td><?= $users['nom'] ?></td>
-                    <td><?= $users['email'] ?></td>
-                    <td><?= $users['numero'] ?></td>
-                    <td><?= $users['rue'] ?></td>
-                    <td><?= $users['code_postal'] ?></td>
-                    <td><?= $users['ville'] ?></td>
-                    <td>
-                        <a href="/?page=edit-user&id=<?= $users['id_utilisateur'] ?>" class="button" title="Modifier">Modifier</a>
-                    </td>
-                    <td>
-                        <form action="/?page=delete-user" method="POST" onsubmit="return confirm('Etes vous sûr de vouloir supprimer cet utilisateur ?')">
-                            <input type="hidden" name="user_id" value="<?= $users['id_utilisateur'] ?>" />
-                            <button class="delete__button" type="submit" title="Supprimer">Supprimer</button>
-                        </form>
-                    </td>
+                    <th>Prénom</th>
+                    <th>Nom</th>
+                    <th>Email</th>
+                    <th>Numéro</th>
+                    <th>Rue</th>
+                    <th>Code Postal</th>
+                    <th>Ville</th>
+                    <th colspan="2">Actions</th>
                 </tr>
-            <?php endforeach; ?>
-            <?php if (empty($users)) : ?>
-                <tr>
-                    <td colspan="9" class="text-user">Aucun utilisateur présent dans la base de données</td>
-                </tr>
-            <?php endif; ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php foreach ($user as $users) : ?>
+                    <tr>
+                        <td><?= $users['prenom'] ?></td>
+                        <td><?= $users['nom'] ?></td>
+                        <td><?= $users['email'] ?></td>
+                        <td><?= $users['numero'] ?></td>
+                        <td><?= $users['rue'] ?></td>
+                        <td><?= $users['code_postal'] ?></td>
+                        <td><?= $users['ville'] ?></td>
+                        <td>
+                            <a href="/?page=edit-user&id=<?= $users['id_utilisateur'] ?>" class="btn button" title="Modifier">Modifier</a>
+                        </td>
+                        <td>
+                            <form action="/?page=delete-user" method="POST" onsubmit="return confirm('Etes vous sûr de vouloir supprimer cet utilisateur ?')">
+                                <input type="hidden" name="user_id" value="<?= $users['id_utilisateur'] ?>" />
+                                <button class="btn btn-danger" type="submit" title="Supprimer">Supprimer</button>
+                            </form>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                <?php if (empty($users)) : ?>
+                    <tr>
+                        <td colspan="9" class="text-user">Aucun utilisateur présent dans la base de données</td>
+                    </tr>
+                <?php endif; ?>
+            </tbody>
+        </table>
+    </div>
 </section>
